8ceda5a99cab3f010a7fa83f284ce4c885298d0a
hyr <seerhyr@163.com>
2019-09-19 14:29:44 +0800

    添加live2d

---
+++ docs/.vuepress/components/Live2D.vue
@@ -0,0 +1,33 @@
+<template >
+</template>
+
+<script>
+import L2Dwidget from "live2d-widget";
+export default {
+    name: "live2D",
+    created() {
+        L2Dwidget.L2Dwidget.init({
+            model: {
+                jsonPath: "/tororo/assets/tororo.model.json"
+            },
+            display: {
+                superSample: 2,
+                width: 150,
+                height: 150,
+                position: "right",
+                hOffset: 0,
+                vOffset: 0
+            },
+            mobile: {
+                show: true,
+                scale: 1,
+                motion: true
+            },
+            react: {
+                opacityDefault: 0.8,
+                opacityOnHover: 0.2
+            }
+        });
+    }
+};
+</script>
